c25f3a73a5b0774ad5e2d336ec030c4f4c0c8a47
FRANCO SALINAS <franco.salinas@agunsa.com>
2023-11-27 00:01:41 -0300

    Se añade vr view

--- scriptServicios.js
+++ scriptServicios.js
@@ -6,3 +6,22 @@ function desplegarTexto( textoEsconder, textoMostrar){
     texto2.style.display = 'block';
 
 }
+
+function openModalDetalle(idModal, categoria){
+    if(categoria == 'restaurante'){
+
+    }
+
+  var modal = document.getElementById("modalDetalle");
+  var span = document.getElementsByClassName("close")[0];
+  modal.style.display = "block";
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+  window.onclick = function(event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+}
